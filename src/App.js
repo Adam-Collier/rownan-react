685a3dd6446de0f86b287b28ed55a938cdda515e
Adam Collier <adamcollierart@gmail.com>
2019-05-29 18:34:40 +0100

    codemirror adds to state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react'
+// import logo from './logo.svg'
+import './App.css'
+import styled from 'styled-components'
+
+import Editor from './Components/Editor'
+
+const AppContainer = styled.div`
+    display: grid;
+    grid-template-columns: 400px 1fr;
+    grid-template-rows: 350px 1fr;
+`
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <AppContainer className="App">
+            <Editor />
+        </AppContainer>
+    )
 }
 
-export default App;
+export default App
