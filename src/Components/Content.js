fdc13f5537fcd0259821b96f4ce6d647b46c0f08
Adam Collier <adamcollierart@gmail.com>
2019-06-13 19:19:10 +0100

    DragDropContext added, handles onDragEnd

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
-import { useAppDispatch } from '../state-context'
+import { useAppDispatch, useAppState } from '../state-context'
 import ContentBlocks from './ContentBlocks'
 import PromoStrip from './PromoStrip'
 import Categories from './Categories'
+
+import { DragDropContext } from 'react-beautiful-dnd'
 
 import { FormButton } from './styles/FormButton'
 
@@ -14,12 +16,35 @@ const ContentContainer = styled.div`
 
 const Content = props => {
   const dispatch = useAppDispatch()
+  const { contentBlocks } = useAppState()
+
+  const onDragEnd = result => {
+    const { destination, source } = result
+    if (
+      destination.droppableID === source.droppableID &&
+      destination.index === source.index
+    ) {
+      return
+    }
+
+    const blocks = contentBlocks
+    const [removed] = blocks.splice(source.index, 1)
+    blocks.splice(destination.index, 0, removed)
+
+    dispatch({
+      type: 'reorderBlocks',
+      reorderedBlocks: blocks
+    })
+    return
+  }
 
   return (
     <ContentContainer>
       <PromoStrip />
       <Categories />
-      <ContentBlocks />
+      <DragDropContext onDragEnd={onDragEnd}>
+        <ContentBlocks />
+      </DragDropContext>
       <FormButton
         type="button"
         value="+"
