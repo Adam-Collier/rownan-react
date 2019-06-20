76a3d2799fd7a24d153b85b0e804de7fcb337e13
Adam Collier <adamcollierart@gmail.com>
2019-06-20 10:18:56 +0100

    only run is extension matches an image

--- src/lib/placeholderImage.js
+++ src/lib/placeholderImage.js
@@ -7,25 +7,27 @@ const sqip = window.require('sqip')
 
 export const placeholderImage = async imgPart => {
   try {
-    let image = `https://media.missguided.co.uk/image/upload${imgPart}`
-    let id = shortid.generate()
+    if (imgPart.match(/\.(jpeg|jpg|gif|png)$/)) {
+      let image = `https://media.missguided.co.uk/image/upload${imgPart}`
+      let id = shortid.generate()
 
-    let imagePath = `${tempDirPath}rownan--temp/${id}.jpeg`
+      let imagePath = `${tempDirPath}rownan--temp/${id}.jpeg`
 
-    const response = await request.get({
-      url: image,
-      encoding: 'binary'
-    })
+      const response = await request.get({
+        url: image,
+        encoding: 'binary'
+      })
 
-    await fs.outputFile(imagePath, response, { encoding: 'binary' })
+      await fs.outputFile(imagePath, response, { encoding: 'binary' })
 
-    const options = {
-      input: imagePath
+      const options = {
+        input: imagePath
+      }
+
+      const result = await sqip.default({ ...options })
+      return result.metadata.dataURIBase64
     }
-
-    const result = await sqip.default({ ...options })
-    console.log(result, 'form inside the function')
-    return result.metadata.dataURIBase64
+    return ''
   } catch (err) {
     console.error(err)
   }
