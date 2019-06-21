e3ff3a02dc94aac02b0a23dc9f75d7d70661712b
Adam Collier <adamcollierart@gmail.com>
2019-06-21 09:46:05 +0100

    fixed error on missing content

--- src/lib/addPlaceholderImageCSS.js
+++ src/lib/addPlaceholderImageCSS.js
@@ -1,33 +1,41 @@
 export default (editorContent, contentBlocks) => {
   let parser = new DOMParser()
   let editorCSS = parser.parseFromString(editorContent, 'text/html')
-  let styleElement = editorCSS.querySelectorAll('style')[0].sheet
 
-  contentBlocks.forEach(({ content: block, type }, i) => {
-    if (block.placeholderImage && type === 'main') {
-      styleElement.cssRules[1].insertRule(
-        `.row${i + 1} { background: url("${
-          block.placeholderImage
-        }"); background-size: contain; background-repeat: no-repeat;
-        }`,
-        i
-      )
+  if (editorCSS.querySelectorAll('style')[0]) {
+    if (!editorCSS.querySelectorAll('style')[0].sheet) {
+      return editorContent
     }
-    if (block.placeholderMobile) {
-      styleElement.cssRules[2].insertRule(
-        `.row${i + 1} { background: url("${
-          block.placeholderMobile
-        }"); background-size: contain; background-repeat: no-repeat;
+
+    let styleElement = editorCSS.querySelectorAll('style')[0].sheet
+
+    contentBlocks.forEach(({ content: block, type }, i) => {
+      if (block.placeholderImage && type === 'main') {
+        styleElement.cssRules[1].insertRule(
+          `.row${i + 1} { background: url("${
+            block.placeholderImage
+          }"); background-size: contain; background-repeat: no-repeat;
         }`,
-        i
-      )
-    }
-  })
+          i
+        )
+      }
+      if (block.placeholderMobile) {
+        styleElement.cssRules[2].insertRule(
+          `.row${i + 1} { background: url("${
+            block.placeholderMobile
+          }"); background-size: contain; background-repeat: no-repeat;
+        }`,
+          i
+        )
+      }
+    })
 
-  let newStyleSheet = document.createElement('style')
-  Array.from(styleElement.cssRules).forEach(rule => {
-    newStyleSheet.innerHTML += rule.cssText
-  })
+    let newStyleSheet = document.createElement('style')
+    Array.from(styleElement.cssRules).forEach(rule => {
+      newStyleSheet.innerHTML += rule.cssText
+    })
 
-  return newStyleSheet.outerHTML
+    return newStyleSheet.outerHTML
+  }
+  return editorContent
 }
