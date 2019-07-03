e25c395a6827341e4806453232db5421a8331e42
Adam Collier <adamcollierart@gmail.com>
2019-07-03 17:13:16 +0100

    srcset created in a loop, widths passed into addImageTransformations

--- src/Components/MainTemplate.js
+++ src/Components/MainTemplate.js
@@ -4,40 +4,50 @@ import addImageTransformations from '../lib/addImageTransformations'
 const MainTemplate = blocks =>
   blocks
     .map((block, index) => {
-      let transformedImage = addImageTransformations(block.image)
-      let transformedMobile = addImageTransformations(block.mobile)
-
-      let transformations = '&qlt=70&fmt.jpeg.interlaced=true'
+      let mobileBreakPoints = [300, 402, 491, 569, 639, 711, 756, 767]
+      let desktopBreakPoints = [
+        768,
+        937,
+        1086,
+        1226,
+        1353,
+        1474,
+        1582,
+        1686,
+        1792,
+        1905
+      ]
 
       return removeEmptyLines`<div class="row fullwidth row${index + 1}">
       <a href="${block.primaryUrl}">
-        ${transformedImage &&
+        ${block.image &&
           `<picture>
             <source media="(max-width: 767px)" sizes="(max-width: 767px) 100vw, 767px" 
-              data-srcset="${transformedMobile}?w=300${transformations} 300w, 
-              ${transformedMobile}?w=402${transformations} 402w, 
-              ${transformedMobile}?w=491${transformations} 491w, 
-              ${transformedMobile}?w=569${transformations} 569w, 
-              ${transformedMobile}?w=639${transformations} 639w, 
-              ${transformedMobile}?w=711${transformations} 711w, 
-              ${transformedMobile}?w=756${transformations} 756w, 
-              ${transformedMobile}?w=767${transformations} 767w">
+              data-srcset="${mobileBreakPoints
+                .map(width => {
+                  return `${addImageTransformations(
+                    block.mobile,
+                    width
+                  )} ${width}w`
+                })
+                .join(',\n              ')}">
             <source sizes="(max-width: 1920px) 100vw, 1920px" 
-            data-srcset="${transformedImage}?w=768 768w, 
-              ${transformedImage}?w=937${transformations} 937w, 
-              ${transformedImage}?w=1086${transformations} 1086w, 
-              ${transformedImage}?w=1226${transformations} 1226w, 
-              ${transformedImage}?w=1353${transformations} 1353w, 
-              ${transformedImage}?w=1474${transformations} 1474w, 
-              ${transformedImage}?w=1582${transformations} 1582w, 
-              ${transformedImage}?w=1686${transformations} 1686w, 
-              ${transformedImage}?w=1792${transformations} 1792w, 
-              ${transformedImage}?w=1905${transformations} 1905w, 
-              ${transformedImage}?w=1920${transformations} 1920w" alt="image failed">
-            <img class="lazyload" data-expand="-50" data-src="${transformedImage}?w=1920${transformations}" src="${
+            data-srcset="${desktopBreakPoints
+              .map(width => {
+                return `${addImageTransformations(
+                  block.image,
+                  width
+                )} ${width}w`
+              })
+              .join(',\n              ')}" 
+              alt="image failed">
+            <img class="lazyload" data-expand="-50" data-src="${addImageTransformations(
+              block.image,
+              '1920'
+            )}" src="${
             block.placeholderImage
               ? block.placeholderImage
-              : `${transformedImage}?w=1920`
+              : `${addImageTransformations(block.image, '1920')}`
           }" alt="backup">
         </picture>`}
         <div class="banner_content center">
