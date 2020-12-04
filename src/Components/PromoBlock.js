bdf214734a2642964e0467519e8464c69b475a6e
Adam Collier <adamcollierart@gmail.com>
2020-12-04 12:05:24 +0000

    chore: removes fragment

--- src/Components/PromoBlock.js
+++ src/Components/PromoBlock.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { useAppDispatch } from '../state-context'
 import { ReactComponent as CloseIcon } from '../icons/close.svg'
 import { convertEmojis } from '../lib/emojiConvert'
