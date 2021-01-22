55d9a13e7a481475b0779f47b3dd773adfc9bc08
Adam Collier <adamcollierart@gmail.com>
2021-01-22 13:27:32 +0000

    feature: app now build with electron-forge

--- public/electron.js
+++ public/electron.js
@@ -1,10 +1,5 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow, Menu, dialog } = require('electron')
-const {
-  default: installExtension,
-  REACT_DEVELOPER_TOOLS
-} = require('electron-devtools-installer')
-const { autoUpdater } = require('electron-updater')
+const { app, BrowserWindow, Menu } = require('electron')
 const browserSync = require('./browsersync')
 const fs = require('fs-extra')
 const menuTemplate = require('./menuTemplate')
@@ -44,77 +39,60 @@ function createWindow() {
   // create the menu
   const menu = Menu.buildFromTemplate(menuTemplate)
   Menu.setApplicationMenu(menu)
-
-  /* Update is ready */
-  autoUpdater.on('update-downloaded', updateInfo => {
-    const dialogOpts = {
-      type: 'info',
-      buttons: ['Restart', 'Later'],
-      title: 'Application Update',
-      message: process.platform === 'win32' ? releaseNotes : releaseName,
-      detail:
-        'A new version has been downloaded. Restart the application to apply the updates.'
-    }
-
-    dialog.showMessageBox(dialogOpts, response => {
-      if (response === 0) autoUpdater.quitAndInstall()
-    })
-  })
-
-  /* Check for updates manually */
-  autoUpdater.checkForUpdates()
-
-  /* Check updates every 10 minutes */
-  setInterval(() => {
-    autoUpdater.checkForUpdates()
-  }, 10 * 60 * 1000)
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
 }
 
-// install dev tools
-const installExtensions = async () => {
-  const installer = require('electron-devtools-installer')
-  const forceDownload = !!process.env.UPGRADE_EXTENSIONS
-  const extensions = ['REACT_DEVELOPER_TOOLS']
-  console.log('devtools installed')
-  return Promise.all(
-    extensions.map(name => installer.default(installer[name], forceDownload))
-  ).catch(console.log)
-}
+// // install dev tools
+// const installExtensions = async () => {
+//   const installer = require('electron-devtools-installer')
+//   const forceDownload = !!process.env.UPGRADE_EXTENSIONS
+//   const extensions = ['REACT_DEVELOPER_TOOLS']
+//   console.log('devtools installed')
+//   return Promise.all(
+//     extensions.map(name => installer.default(installer[name], forceDownload))
+//   ).catch(console.log)
+// }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', async () => {
-  // install the devtools
-  if (process.env.ELECTRON_START_URL) await installExtensions()
-  createWindow()
-})
+// app.on('ready', async () => {
+//   // install the devtools
+//   if (process.env.ELECTRON_START_URL) await installExtensions()
+//   createWindow()
+// })
 
-app.whenReady().then(() => {
-  installExtension(REACT_DEVELOPER_TOOLS)
-    .then(name => console.log(`Added Extension:  ${name}`))
-    .catch(err => console.log('An error occurred: ', err))
-})
+app
+  .whenReady()
+  .then(() => {
+    if (process.env.ELECTRON_START_URL) {
+      const {
+        default: installExtension,
+        REACT_DEVELOPER_TOOLS
+      } = require('electron-devtools-installer')
+
+      installExtension(REACT_DEVELOPER_TOOLS)
+        .then(name => console.log(`Added Extension:  ${name}`))
+        .catch(err => console.log('An error occurred: ', err))
+    }
+    return
+  })
+  .then(createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
 
 app.on('activate', function() {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow()
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
 })
 
 // In this file you can include the rest of your app's specific main process
