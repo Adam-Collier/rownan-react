763ed82a410c9de24081f8514e51492d95b8e4a6
Adam Collier <adamcollierart@gmail.com>
2021-05-24 12:27:17 +0100

    refactor: add the styles needed for the image placeholders to work

--- src/context/actions/default-editor-code.js
+++ src/context/actions/default-editor-code.js
@@ -1,4 +1,29 @@
 const defaultEditorCode = `<style>
+  .row-placeholder {
+    position: relative;
+    overflow: hidden;
+    height: 0;
+    width: 100%;
+    background-color: #ECECF2;
+    padding-top: 44.79%;
+  }
+  .row img {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      max-width: none;
+      transform: translateZ(0);
+      transition: opacity 0.5s linear;
+      will-change: opacity;
+  }
+  @media(max-width: 767px){
+      .row-placeholder {
+          padding-top: 100%;
+      }
+  }
   .title1,
   .title3,
   .subtitle1,
