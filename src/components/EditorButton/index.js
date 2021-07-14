07523732d5b08c15e1179afc8ac6305606f4b9f1
Adam Collier <adamcollierart@gmail.com>
2021-07-14 15:17:39 +0100

    fix: path for child process

--- src/components/EditorButton/index.js
+++ src/components/EditorButton/index.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { ReactComponent as FileIcon } from '../../icons/file-text.svg'
 
 const { exec } = window.require('child_process')
+// without fix-path the exec command will fail in prod
+const fixPath = window.require('fix-path')
+
+fixPath()
 
 let handleClick = (path) => {
   let folderPath = path.substring(0, path.lastIndexOf('/'))
